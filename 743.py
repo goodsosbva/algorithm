2f92a77b0a4f3ec7e7c97e2eca25a05a4887e2ef
goodsosbva <62534722+goodsosbva@users.noreply.github.com>
2021-03-24 23:19:02 +0900

    Update 743.py

--- 743.py
+++ 743.py
@@ -8,11 +8,11 @@ def networkDelayTime(times: List[List[int]], N: int, K: int) -> int:
     # 그래프 인접 리스트 구성
     for u, v, w in times:
         graph[u].append((v, w))
-    print(graph)
+    # print(graph)
     # 큐 변수: [(소요 시간, 정점)]
     Q = [(0, K)]
     dist = collections.defaultdict(int)
-    print(Q)
+    # print(Q)
 
     # 우선순위 큐 최솟값 기준으로 정점까지 최단 경로 삽입
     while Q:
@@ -21,11 +21,11 @@ def networkDelayTime(times: List[List[int]], N: int, K: int) -> int:
             dist[node] = time
             for v, w in graph[node]:
                 alt = time + w
-                print(Q)
+                # print(Q)
                 heapq.heappush(Q, (alt, v))
 
     # 모든 노드의 최단 경로 존재 여부 판별
-    print(dist)
+    # print(dist)
     if len(dist) == N:
         return max(dist.values())
     return -1
